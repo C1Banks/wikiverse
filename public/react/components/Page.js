317fad5f4a4d52cc57a7d066c3433e9156156096
C1Banks <129791524+C1Banks@users.noreply.github.com>
2023-05-19 13:57:44 -0700

    page requests and authorId were added

--- public/react/components/Page.js
+++ public/react/components/Page.js
@@ -8,28 +8,28 @@ import { PagesList } from './PagesList';
 export const Page = (props) => {
   const [clicked, setClick] = useState(false)
 
-  async function fetchPageData(){
-    try{
-      const getInfo = await fetch(`${apiURL}/wiki/:${props.page.slug}`) 
-      const pageData = await getInfo.json();
-      props.setPage(pageData);
+  //async function fetchPageData(){
+    //try{
+      //const getInfo = await fetch(`${apiURL}/wiki/:${props.page.slug}`) 
+      //const pageData = await getInfo.json();
+      //props.setPage(pageData);
 
       
-    }catch(err){
-      console.log("page unable to load!!!");
-    }
-  }
+    //}catch(err){
+      //console.log("page unable to load!!!");
+    //}
+  //}
   console.log(props);
+  console.log(props.page.authorId)
 
 
   return ( <>
     
-    <h3> <button onClick= { () => setClick(true) } >{props.page.title}</button></h3>
+    <h3> <button onClick= { () => setClick(!clicked) } >{props.page.title}</button></h3>
     {clicked && (
       <ol>
-        {props.page.id}
-        {props.page.content}
-      </ol>
+        {props.page.content}     
+        </ol>
     )}
   </>)
 } 
